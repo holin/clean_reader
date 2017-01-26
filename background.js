ade12acf71fd488070b20357042f12b1be3700fe
Holin <holin.he@gmail.com>
2017-01-26 13:44:25 +0800

    Update icon status for each tab

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 new (function($) {
+
 	function init($) {
 
 		var off_status = {}
@@ -13,32 +14,49 @@ new (function($) {
 			"38": "assets/icon38_off.png"
 		};
 		chrome.browserAction.setIcon({
-			path: on_path
+			path: off_path
+		});
+
+
+		function update_icon_for_tab(tabId) {
+			var is_off = off_status[tabId];
+			if (is_off === undefined) {
+				is_off = true;
+			}
+			console.log("update_icon_for_tab", tabId, off_status, is_off);
+			if (is_off) {
+				chrome.browserAction.setIcon({
+					path: off_path
+				});
+				chrome.browserAction.setBadgeText({
+					tabId: tabId,
+					text: ""
+				});
+			} else {
+				chrome.browserAction.setIcon({
+					path: on_path
+				});
+				chrome.browserAction.setBadgeText({
+					tabId: tabId,
+					text: "on"
+				});
+			}
+		}
+
+		chrome.tabs.onActivated.addListener( function(info) {
+				var tabId = info.tabId;
+				update_icon_for_tab(tabId);
 		});
 
 		chrome.extension.onMessage.addListener(
 			function(request, sender, sendResponse) {
-				off_status[sender.tab.id] = request.is_off
-				// if (request.is_off) {
-				// 	chrome.browserAction.setIcon({
-				// 		path: off_path
-				// 	});
-				// } else {
-				// 	chrome.browserAction.setIcon({
-				// 		path: on_path
-				// 	});
-				// }
-				chrome.browserAction.setBadgeText({
-					tabId: sender.tab.id,
-					text: request.is_off ? "" : "on"
-				});
+				var tabId = sender.tab.id;
+				off_status[tabId] = request.is_off
+				update_icon_for_tab(tabId);
  			}
 		);
 
-		// Called when the user clicks on the browser action.
 		chrome.browserAction.onClicked.addListener(function(tab) {
-			// No tabs or host permissions needed!
-			console.log('Turning ' + tab.url + ' red!');
 			chrome.tabs.executeScript(
 				null,
 				{file: "libs/jquery.min.js"}
