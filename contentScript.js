66f4ec65269a32540a96521deb8d80e7805cd213
Holin <holin.he@gmail.com>
2017-02-03 17:49:25 +0800

    Only trigger toggle after Usage:   rails new APP_PATH [options]
    Options:
      -r, [--ruby=PATH]                                      # Path to the Ruby binary of your choice
                                                             # Default: /Users/holin/.rbenv/versions/2.3.1/bin/ruby
      -m, [--template=TEMPLATE]                              # Path to some application template (can be a filesystem path or URL)
      -d, [--database=DATABASE]                              # Preconfigure for selected database (options: mysql/oracle/postgresql/sqlite3/frontbase/ibm_db/sqlserver/jdbcmysql/jdbcsqlite3/jdbcpostgresql/jdbc)
                                                             # Default: sqlite3
      -j, [--javascript=JAVASCRIPT]                          # Preconfigure for selected JavaScript library
                                                             # Default: jquery
          [--skip-gemfile], [--no-skip-gemfile]              # Don't create a Gemfile
      -B, [--skip-bundle], [--no-skip-bundle]                # Don't run bundle install
      -G, [--skip-git], [--no-skip-git]                      # Skip .gitignore file
          [--skip-keeps], [--no-skip-keeps]                  # Skip source control .keep files
      -M, [--skip-action-mailer], [--no-skip-action-mailer]  # Skip Action Mailer files
      -O, [--skip-active-record], [--no-skip-active-record]  # Skip Active Record files
      -P, [--skip-puma], [--no-skip-puma]                    # Skip Puma related files
      -C, [--skip-action-cable], [--no-skip-action-cable]    # Skip Action Cable files
      -S, [--skip-sprockets], [--no-skip-sprockets]          # Skip Sprockets files
          [--skip-spring], [--no-skip-spring]                # Don't install Spring application preloader
          [--skip-listen], [--no-skip-listen]                # Don't generate configuration that depends on the listen gem
      -J, [--skip-javascript], [--no-skip-javascript]        # Skip JavaScript files
          [--skip-turbolinks], [--no-skip-turbolinks]        # Skip turbolinks gem
      -T, [--skip-test], [--no-skip-test]                    # Skip test files
          [--dev], [--no-dev]                                # Setup the application with Gemfile pointing to your Rails checkout
          [--edge], [--no-edge]                              # Setup the application with Gemfile pointing to Rails repository
          [--rc=RC]                                          # Path to file containing extra configuration options for rails command
          [--no-rc], [--no-no-rc]                            # Skip loading of extra configuration options from .railsrc file
          [--api], [--no-api]                                # Preconfigure smaller stack for API only apps
    
    Runtime options:
      -f, [--force]                    # Overwrite files that already exist
      -p, [--pretend], [--no-pretend]  # Run but do not make any changes
      -q, [--quiet], [--no-quiet]      # Suppress status output
      -s, [--skip], [--no-skip]        # Skip files that already exist
    
    Rails options:
      -h, [--help], [--no-help]        # Show this help message and quit
      -v, [--version], [--no-version]  # Show Rails version number and quit
    
    Description:
        The 'rails new' command creates a new Rails application with a default
        directory structure and configuration at the path you specify.
    
        You can specify extra command-line arguments to be used every time
        'rails new' runs in the .railsrc configuration file in your home directory.
    
        Note that the arguments specified in the .railsrc file don't affect the
        defaults values shown above in this help message.
    
    Example:
        rails new ~/Code/Ruby/weblog
    
        This generates a skeletal Rails installation in ~/Code/Ruby/weblog. press more then once in one second.

--- contentScript.js
+++ contentScript.js
@@ -1,9 +1,17 @@
 new (function($) {
 	function init($) {
+		var toggle_key_press_count = 0;
 		$(document).keyup(function(e) {
 				// console.debug("content script key code", e.keyCode)
-				if (e.keyCode == 82) { //r
+				if (e.keyCode == 82) { //keypess: r
+					toggle_key_press_count += 1
+					setTimeout(function() {
+						toggle_key_press_count = 0
+					}, 1000);
+					if (toggle_key_press_count > 1) {
+						//only trigger toggle after `r` press more then once in one second.
 						Reader.toggle();
+					}
 				}
 		});
 	}
